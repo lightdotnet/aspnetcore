04b48dc5300143a7c1ef30ac6566cb2547a7914d
minh.vu <minhvu.box@gmail.com>
2025-11-05 13:27:50 +0700

    update Identity Claim extensions

---
+++ src/Authorization/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Light.Authorization;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDefaultPermissionManager(this IServiceCollection services) =>
+        services.AddSingleton<PermissionManager>();
+
+    public static IServiceCollection AddPermissionManager<T>(this IServiceCollection services)
+        where T : PermissionManager
+    {
+        return services.AddSingleton<PermissionManager, T>();
+    }
+
+    public static IServiceCollection AddPermissionPolicy(this IServiceCollection services) =>
+        services.AddSingleton<IAuthorizationPolicyProvider, PermissionPolicyProvider>();
+
+    public static IServiceCollection AddPermissionAuthorization<T>(this IServiceCollection services)
+        where T : class, IAuthorizationHandler
+    {
+        services.AddScoped<IAuthorizationHandler, T>();
+
+        return services;
+    }
+        
+}
