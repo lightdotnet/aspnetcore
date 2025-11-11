fe5c23767f6e1232c7e83ca2d118e551d642ff36
minh.vu <minhvu.box@gmail.com>
2025-11-11 11:08:38 +0700

    update sample

--- src/Authorization/PermissionManager.cs
+++ src/Authorization/PermissionManager.cs
@@ -5,7 +5,7 @@ public class PermissionManager
     /// <summary>
     /// default all (include undefined) permissions are valid
     /// if you want to restrict permissions, override this method
-    ///     and register your own PermissionManager with .AddPermissionStores<T>
+    ///     and register your own PermissionManager with .AddPermissionManager<T>
     /// </summary>
     public virtual Task<bool> IsValidAsync(string permission) => Task.FromResult(true);
 }
